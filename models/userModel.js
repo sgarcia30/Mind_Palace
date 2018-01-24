46e25f342a08d89350aed0e69f350019a8fc72b8
sgarcia30 <garciatres@gmail.com>
2018-01-24 18:04:32 -0500

    reorganized some files

--- models/userModel.js
+++ models/userModel.js
@@ -32,13 +32,13 @@ UserSchema.methods.serialize = function () {
 	};
 };
 
-UserSchema.methods.validatePassword = function(password) {
-	return bcrypt.compare(password, this.password);
-};
+// UserSchema.methods.validatePassword = function(password) {
+// 	return bcrypt.compare(password, this.password);
+// };
 
-UserSchema.statics.hashPassword = function(password) {
-	return bcrypt.hash(password, 10);
-};
+// UserSchema.statics.hashPassword = function(password) {
+// 	return bcrypt.hash(password, 10);
+// };
 
 const User = mongoose.model('User', UserSchema);
 
