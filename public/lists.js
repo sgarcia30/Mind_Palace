2f06492c6010a8b5a8671d37c4a67134b70f5d9b
sgarcia30 <garciatres@gmail.com>
2018-02-08 13:56:58 -0500

    cleaned up code and added nav button

--- public/lists.js
+++ public/lists.js
@@ -1,35 +1,29 @@
-function handleSubmitButton() {
-	$('.listForm').on('submit', function(event) {
-		event.preventDefault();
-		const title = $('#listType').val();
-		const date = $('#date').val();
-		const category = $('#category').val();
-        const userId = localStorage.getItem('userId');
+const userId = localStorage.getItem('userId');
 
-		const settings = {
-    		url: 'http://localhost:8080/api/users/list',
-    		data: JSON.stringify({
-    			title: title,
-    			date: date,
-    			category: category,
-                userId: userId
-    			}),
-    		dataType: 'json',
-    		contentType: 'application/json',
-    		type: 'PUT',
-            error: function(error) {
-                console.log(error)
-            },
-    		success: function(response) {
-                console.log(response);
-                localStorage.setItem('listTitle', title);
-                window.location = "listBuilding.html";
-            }
-    		}
-
-        // Make call to API with ajax
-        $.ajax(settings);
-  	});
+const settings = {
+	url: `http://localhost:8080/api/users/${userId}/list`,
+	dataType: 'json',
+	contentType: 'application/json',
+	type: 'GET',
+	error: function(error) {
+		console.log(error);
+	},
+	success: getListTitles
 }
 
-$(handleSubmitButton);
+function getListTitles(data) {
+	const lists = data.lists;
+	
+	lists.forEach( function(list) {
+		const listTitle = list.title;
+		const val = renderListVal(listTitle);
+		$('.listVals').append(val);
+	})
+}
+
+function renderListVal(val) {
+	return `
+		<li>${val}</li>`
+}
+
+$.ajax(settings);
