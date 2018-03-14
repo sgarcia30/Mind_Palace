7f3f201ace364af49ce380e51f4ade00133ff1e9
sgarcia30 <garciatres@gmail.com>
2018-03-14 11:18:40 -0400

    finishing touches for review

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ exports.DATABASE_URL ||
 	process.env.DATABASE_URL ||
 	global.DATABASE_URL;
 
-
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || "mind-palace";
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY  || "7d";
